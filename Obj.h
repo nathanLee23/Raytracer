8381285cbe2ce7b1b5758efe713b65376044fb19
nathanLee23 <z5205432@student.unsw.edu.au>
2021-01-02 00:45:22 +1100

    added volumetric mediums

--- Obj.h
+++ Obj.h
@@ -1,9 +1,20 @@
 #pragma once
+
 #include <random>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 
 #include "Vec3.h"
 #include "Ray.h"
 #include "Material.h"
+#include "globals.h"
+
+
+float sgn(float x) {
+	return (float)((x > 0) - (x < 0));
+}
+
 
 class Obj {
 public:
@@ -17,27 +28,203 @@ public:
 	virtual Vec3 normal(Vec3 p) = 0;
 	virtual Vec3 samplePoint(std::mt19937 gen) = 0;
 };
-
+//
+//std::uniform_real_distribution<float> eta(0, 1);
+//std::uniform_int_distribution<int> side(0, 5);
 class Box : public Obj {
 public:
 	Vec3 min;
 	Vec3 max;
 
-	Box(Vec3 min, Vec3 max);
-	float intersect(Ray& ray);
-	Vec3 normal(Vec3 p);
-	Vec3 samplePoint(std::mt19937 gen);
+	Box(Vec3 min, Vec3 max) : min(min), max(max) {
+	}
+
+	float intersect(Ray& ray) {
+		float t1 = (min.x - ray.o.x) / ray.d.x;
+		float t2 = (max.x - ray.o.x) / ray.d.x;
+
+		float tmin = std::min(t1, t2);
+		float tmax = std::max(t1, t2);
+
+		t1 = (min.y - ray.o.y) / ray.d.y;
+		t2 = (max.y - ray.o.y) / ray.d.y;
+
+		tmin = std::max(tmin, std::min(t1, t2));
+		tmax = std::min(tmax, std::max(t1, t2));
+		t1 = (min.z - ray.o.z) / ray.d.z;
+		t2 = (max.z - ray.o.z) / ray.d.z;
+
+		tmin = std::max(tmin, std::min(t1, t2));
+		tmax = std::min(tmax, std::max(t1, t2));
+		//for (int i = 1; i < 3; i++) {
+		//	t1 = (min[i] - ray.o[i])/ray.d[i];
+		//	t2 = (max[i] - ray.o[i])/ray.d[i];
+
+		//	tmin = std::max(tmin, std::min(t1, t2));
+		//	tmax = std::min(tmax, std::max(t1, t2));
+		//}
+		return tmax >= tmin ? (tmin > EPS ? tmin : tmax) : INFINITY;
+	}
+
+	Vec3 normal(Vec3 p) {
+		Vec3 c = (max + min) / 2.0f;
+		Vec3 cToP = p - c;
+
+		// Can optimize this further
+		int argMax = 0;
+		for (int i = 1; i < 3; i++) {
+			if (abs(cToP[i])*(max[argMax] - min[argMax]) > abs(cToP[argMax]) * (max[i] - min[i])) {
+				argMax = i;
+			}
+		}
+
+		return Vec3(
+			(argMax == 0) * sgn(cToP.x),
+			(argMax == 1) * sgn(cToP.y),
+			(argMax == 2) * sgn(cToP.z)
+		);
+	}
+
+
+	Vec3 samplePoint(std::mt19937 gen) {
+
+		std::uniform_real_distribution<float> rx(min.x, max.x);
+		std::uniform_real_distribution<float> rz(min.z, max.z);
+
+		return Vec3(rx(gen), max.y, rz(gen));
+	}
 };
+
+class Triangle : public Obj {
+public:
+	Vec3 a;
+	Vec3 b;
+	Vec3 c;
+	Vec3 n;
+
+	Triangle(Vec3 a, Vec3 b, Vec3 c) : a(a), b(b), c(c), n((b - a).cross(c - b).normalized()) {
+	}
+
+	float intersect(Ray& ray) {
+		return 0.0;
+	}
+	Vec3 normal(Vec3 p) {
+		return n;
+	}
+	Vec3 samplePoint(std::mt19937 gen) {
+		return Vec3();
+	}
+};
+
+class Mesh : public Obj {
+public:
+	std::vector<Triangle> triangles;
+	Box bounds = Box(Vec3(), Vec3());
+
+	Mesh() {
+	}
+
+	void  add_triangle(Triangle t) {
+		triangles.push_back(t);
+	}
+	float intersect(Ray& ray) {
+		if (triangles.empty()) {
+			return INFINITY;
+		}
+		float min_t = INFINITY;
+		Triangle min_triangle = triangles.front();
+		for (Triangle triangle : triangles) {
+			float t = triangle.intersect(ray);
+			if (t < min_t) {
+				min_t = t;
+				min_triangle = triangle;
+			}
+		}
+		return min_t;
+	}
+	Vec3 normal(Vec3 p) {
+		return Vec3();
+	}
+	Vec3 samplePoint(std::mt19937 gen) {
+		return Vec3();
+	}
+};
+
+int filter_extra(std::istringstream &s) {
+	std::string x;
+	if (s >> x) {
+		return std::stoi(x.substr(0, x.find_first_of('/')));
+	} else {
+		return -1;
+	}
+
+}
+
+Mesh load_mesh(std::string file_name) {
+	std::ifstream ifile(file_name);
+	if (errno) {
+		throw std::system_error(errno, std::system_category(), "Failed to open: " + file_name);
+	} else {
+		std::cout << "Successfully opened file " + file_name << std::endl;
+	}
+	Mesh mesh = Mesh();
+
+	
+	std::vector<Vec3> vertices;
+	while (!ifile.eof()) {
+		if (isspace(ifile.peek())) {
+			ifile.get();
+			continue;
+		}
+		std::string line;
+		std::getline(ifile, line);
+		if (line.size() < 2) { // Too short
+		} else if (line.rfind("#", 0) == 0) { // comment
+		} else if (line.rfind("v ", 0) == 0) { // vertex
+			Vec3 vertex;
+			sscanf_s(line.c_str(), "v %f %f %f", &vertex.x, &vertex.y, &vertex.z);
+			vertices.push_back(vertex);
+		} else if (line.rfind("f ", 0) == 0) {//face
+			std::istringstream line_ss(line);
+			std::string word;
+			line_ss >> word;
+
+			int a = filter_extra(line_ss);
+			int b = filter_extra(line_ss);
+			int c = filter_extra(line_ss);
+
+			mesh.add_triangle(Triangle(vertices.at(a - 1), vertices.at(b - 1), vertices.at(c - 1)));
+			b = c;
+			while (c = filter_extra(line_ss) >= 0) {
+				mesh.add_triangle(Triangle(vertices.at(a - 1), vertices.at(b - 1), vertices.at(c - 1)));
+				b = c;
+			}
+		}
+	}
+	return mesh;
+}
 
 class Plane : public Obj {
 public:
 	float k; // k = a.dot(n)
 	Vec3 n;
 
-	Plane(Vec3 a, Vec3 n);
-	float intersect(Ray& ray);
-	Vec3 normal(Vec3 p);
-	Vec3 samplePoint(std::mt19937 gen);
+	Plane(Vec3 a, Vec3 n_) {
+		n = n_.normalized();
+		k = n.dot(a);
+	}
+
+	float intersect(Ray& ray) {
+		return (k - n.dot(ray.o)) / n.dot(ray.d);
+	}
+
+	Vec3 normal(Vec3 p) {
+		return n;
+	
+	}
+	Vec3 samplePoint(std::mt19937 gen) {
+		return Vec3();
+	}
 };
 
 class Sphere : public Obj {
@@ -45,8 +232,25 @@ public:
 	Vec3 c;
 	float r;
 
-	Sphere(Vec3 c, float r);
-	float intersect(Ray& ray);
-	Vec3 normal(Vec3 p);
-	Vec3 samplePoint(std::mt19937 gen);
+	Sphere(Vec3 c, float r) : c(c), r(r) {
+	}
+	float intersect(Ray& ray) {
+		float B = 2 * ray.d.dot(ray.o - c);
+		float C = (ray.o - c).sqrNorm() - r * r;
+
+		float sqrDiscr = B * B - 4 * C;
+		if (sqrDiscr < 0.0f) {
+			return INFINITY;
+		}
+		float discr = sqrt(sqrDiscr);
+		float t1 = (-B - discr) / 2.0f;
+		//float t2 = (-B + discr)/2.0f;
+		return t1 > EPS ? t1 : (-B + discr) / 2.0f;
+	}
+	Vec3 normal(Vec3 p) {
+		return (p - c) / r;
+	}
+	Vec3 samplePoint(std::mt19937 gen) {
+		return Vec3();
+	}
 };
--- Raytracer.cpp
+++ Raytracer.cpp
@@ -16,6 +16,7 @@
 #include <chrono>
 #include <ctime>  
 
+#include <omp.h>
 #include <SFML/Graphics.hpp>
 
 #include "Vec3.h"
@@ -25,28 +26,19 @@
 #include "Obj.h"
 #include "globals.h"
 
-#define WIDTH 1000
-#define HEIGHT 1000
+#define WIDTH 800
+#define HEIGHT 800
 #define FOVX 120.0f
-#define ACTIVE_SCENE 1
+#define ACTIVE_SCENE 3
 
 #define AMBIENT 0.3f
-#define MIN_BOUNCES 2
 #define SAMPLES_PER_PIXEL 1
+#define THREADS 2
+
+// Unused
+#define MIN_BOUNCES 2
 #define BOUNCE_PROB 0.8f
 #define HEMISPHERE_AREA (M_PI*2.0f)
-
-/*
-Things to try to remove noise
-- Implement brightness based russian roulette
-- better self intersection handling
-- Investigate better light handling
-  - Black body light? (basically just stop when the ray hits it)
-  - Why does specular lighting cause noise?
-  - Why does diffuse lighting make the light appear dark?
-*/
-
-
 
 using namespace std;
 
@@ -75,7 +67,6 @@ public:
 struct Intersection {
 	float t;
 	Obj * hitObj;
-	Material material;
 };
 
 class Scene {
@@ -91,7 +82,7 @@ public:
 
 	Intersection castRay(Ray& ray) {
 		float minT = INFINITY;
-		Obj *hitObj = NULL;
+		Obj *hitObj = nullptr;
 		for (Obj *obj : objs) {
 			float t = obj->intersect(ray);
 			if (t > EPS && t < minT) {
@@ -101,17 +92,18 @@ public:
 		}
 		return {minT, hitObj};
 	}
+/*
+	float mesh_intersect(Ray &ray, Vec3 &n, Obj **hitObj) {
 
-
+	}*/
 };
 
 Scene scene;
 
-mt19937 gen;
 uniform_real_distribution<float> pix(-0.5f, 0.5f);
 uniform_real_distribution<float> uniform01(0.0f, 1.0f);
-uniform_real_distribution<float> uniformAngle(0.0f, 2*M_PI);
-Obj *light = NULL;
+uniform_real_distribution<float> uniformAngle(0.0f, 2.0f*M_PI);
+Obj *light = nullptr;
 void buildScene(int i) {
 	Material mirrorMat = { Vec3(1.0f), 0.0f, Surface(reflective) };
 	Material diffuseMat = { Vec3(0.73f, 0.73f, 0.73f), 0.0f, Surface(diffuse) };
@@ -120,7 +112,7 @@ void buildScene(int i) {
 	Material specularMat = { Vec3(1.0f), 0.0f, Surface(specular) };
 
 	// Using specular light sauces creates a lot of noise
-	Material lightMat = { Vec3(1.0f), 4.0f, Surface(reflective) };
+	Material lightMat = { Vec3(1.0f), 16.0f, Surface(specular) };
 	Material ovenMat = { Vec3(0.5f), 0.5f, Surface(diffuse) };
 	switch (i) {
 	default:
@@ -139,9 +131,8 @@ void buildScene(int i) {
 		scene.addObject(new Plane(Vec3(800.0f, 0.0f, 0.0f), Vec3(-1.0f, 0.0f, 0.0)), greenMat);
 		scene.addObject(new Plane(Vec3(-800.0f, 0.0f, 0.0f), Vec3(1.0f, 0.0f, 0.0)), redMat);
 
-		scene.addObject(new Box(Vec3(-600.0f, 400.0f, -600.0f), Vec3(-400.0f, 800.0f, -400.0f)), specularMat);
 		scene.addObject(new Sphere(Vec3(400.0f, 440.0f, -600.0f), 200.3f), specularMat);
-		//scene.addObject(new Box(Vec3(-200.0f, -800.0f, -500.0f), Vec3(200.0f, -750.0f, -100.0f)), diffuseMat);
+		scene.addObject(new Box(Vec3(-200.0f, -800.0f, -500.0f), Vec3(200.0f, -750.0f, -100.0f)), specularMat);
 
 		scene.addObject(new Box(Vec3(-200.0f, -800.0f, -500.0f), Vec3(200.0f, /*-795.0f*/ -799.9f, -100.0f)), lightMat);
 		light = scene.objs.back();
@@ -158,6 +149,17 @@ void buildScene(int i) {
 		scene.addObject(new Plane(Vec3(800.0f, 0.0f, 0.0f), Vec3(-1.0f, 0.0f, 0.0)), ovenMat);
 		scene.addObject(new Plane(Vec3(-800.0f, 0.0f, 0.0f), Vec3(1.0f, 0.0f, 0.0)), ovenMat);
 		break;
+	case 3:
+		scene.addObject(new Plane(Vec3(0.0f, 0.0f, -800.0f), Vec3(0.0f, 0.0f, 1.0)), diffuseMat);
+		scene.addObject(new Plane(Vec3(0.0f, 0.0f, 800.0f), Vec3(0.0f, 0.0f, -1.0)), diffuseMat);
+		scene.addObject(new Plane(Vec3(0.0f, 800.0f, 0.0f), Vec3(0.0f, -1.0f, 0.0f)), diffuseMat);
+		scene.addObject(new Plane(Vec3(0.0f, -800.0f, 0.0f), Vec3(0.0f, 1.0f, 0.0)), diffuseMat);
+		scene.addObject(new Plane(Vec3(800.0f, 0.0f, 0.0f), Vec3(-1.0f, 0.0f, 0.0)), greenMat);
+		scene.addObject(new Plane(Vec3(-800.0f, 0.0f, 0.0f), Vec3(1.0f, 0.0f, 0.0)), redMat);
+
+		scene.addObject(new Sphere(Vec3(0.0f, -040.0f, -500.0f), 200.3f), diffuseMat);
+		scene.addObject(new Box(Vec3(-50.0f, -800.0f, -650.0f), Vec3(50.0f, /*-795.0f*/ -799.9f, -550.0f)), lightMat);
+		light = scene.objs.back();
 	}
 	//scene.addObject(new Box(Vec3(1.0f, 1.0f, -3.0f), Vec3(1.0f, 1.0f, -3.0f) + Vec3(1.0f, 1.0f, 0.0f)), diffuseMat);
 	/*
@@ -172,17 +174,17 @@ void buildScene(int i) {
 // Transform pixel coordinates to perspective rays
 // Camera is at (0,0,0) facing (0,0,-1)
 void cameraRay(float px, float py, Ray& ray) {
-	float x = (2.0f * px - WIDTH) / WIDTH * tan(FOVX * M_PI / 180.0f / 2.0f);
+	float x = (2.0f * px - WIDTH) / WIDTH * tanf(FOVX * M_PI / 180.0f / 2.0f);
 	// https://computergraphics.stackexchange.com/questions/8479/how-to-calculate-ray
-	float y = (2.0f * py - HEIGHT) / HEIGHT * tan(((float) HEIGHT) / WIDTH * FOVX * M_PI / 180.0f / 2.0f);
+	float y = (2.0f * py - HEIGHT) / HEIGHT * tanf(((float) HEIGHT) / WIDTH * FOVX * M_PI / 180.0f / 2.0f);
 	float z = -1.0f;
-	ray.o = Vec3();
+	ray.o = Vec3(0.0,0.0f,0.0f);
 	ray.d = Vec3(x,y,z).normalized();
 
 	//return Ray(Vec3(), Vec3(x, y, z));
 }
 
-Vec3 cosineSampleHemisphere() {
+Vec3 cosineSampleHemisphere(mt19937 &gen) {
 	float r_sqr = uniform01(gen);
 	float phi = uniformAngle(gen);
 	float r = sqrt(r_sqr);
@@ -190,11 +192,21 @@ Vec3 cosineSampleHemisphere() {
 	float y = sin(phi)*r;
 	return Vec3(x, y, sqrt(1.0f - x*x - y*y));
 }
-Vec3 uniformSampleHemisphere() {
+Vec3 uniformSampleHemisphere(mt19937 &gen) {
 	float z = uniform01(gen);
 	float theta = uniformAngle(gen);
 	float r = sqrt(1.0f-z*z);
 	return Vec3(cos(theta)*r, sin(theta)*r, z);
+}
+
+// Creates an orthonormal basis
+void onb(Vec3 v1, Vec3 &v2, Vec3 &v3) {
+	if (abs(v1.x) > abs(v1.y)) {
+		v2 = Vec3(-v1.z, 0.0f, v1.x) / sqrt(v1.z*v1.z + v1.x*v1.x);
+	} else {
+		v2 = Vec3(0.0f, v1.z, -v1.y) / sqrt(v1.z*v1.z + v1.y*v1.y);
+	}
+	v3 = v1.cross(v2);
 }
 
 // cos_t is the dot product of the normal and the incident vectors
@@ -206,11 +218,30 @@ float schlickApprox(float r, float cos_t) {
 	return R0 + (1.0f - R0)*x2*x2*x;
 }
 
+Vec3 henyey_greenstein(Vec3 in, float g, mt19937 &gen) {
+	float sqr_part = (1.0f - g * g) / (1.0f + g * g + 2.0f*g*uniform01(gen));
+	float cos_t = -(1.0f + g * g - (sqr_part*sqr_part)) / 2.0f / g;
+	float sin_t = sqrtf(max(0.0f, 1.0f - cos_t*cos_t));
+	float phi = uniformAngle(gen);
+	Vec3 v2, v3;
+	onb(in, v2, v3);
+	//v3.normalized();
+	//cout << in.norm() << endl;
+	return v2 * sin_t*cos(phi) + v3 * sin_t*sin(phi) + in * cos_t;
+}
+float henyey_greenstein_p(float cos_t, float g=1.0f) {
+	return 1/4.0f/M_PI*(1.0f-g*g)/(1.0f+g*g+2.0f*g*powf(cos_t,1.5f));
+}
+
+Vec3 direction(float theta, float phi) {
+	return Vec3(cos(theta)*sin(phi), sin(theta)*sin(phi), cos(phi));
+}
+
 int c = 0;
-Vec3 rayTrace(Ray& ray) {
+Vec3 rayTrace(Ray& ray, mt19937 &gen) {
 	Vec3 attenuation = Vec3(1.0f);
 	Vec3 color = Vec3(0.0f);
-	Obj *last = NULL;
+	Obj *last = nullptr;
 	//int i = 0;
 	int a = 0;
 	float mis_brdf_pdf = -1.0f; // Negative when mis was not used
@@ -219,38 +250,69 @@ Vec3 rayTrace(Ray& ray) {
 		
 		//TODO: Weigh this by MIS
 		if (isinf(intersection.t)) {
-			float a = ray.d.dot(Vec3(0.2, -0.8, -0.4).normalized());
+			float a = ray.d.dot(Vec3(0.2f, -0.8f, -0.4f).normalized());
 			if (a > 0.999) {
 
 				color = color + attenuation*Vec3(5);
 			} else if (a > 0.96) {
 				color = color + attenuation*Vec3(5)*(a-0.96f)*(a - 0.96f)/ (0.999f - 0.96f) /(0.999f-0.96f);
 			}
-			color = color + attenuation*Vec3(0.5,0.70,0.8);
+			color = color + attenuation*Vec3(0.5f,0.70f,0.8f);
 			break;
 		}
 
+		//float density = 0.0005f;
+		float density = 0.0f;
+
 		Vec3 hp = intersection.t*ray.d + ray.o;
-		ray.o = hp;
 
 		Obj *obj = intersection.hitObj;
 		Vec3 n = obj->normal(hp);
 		Vec3 emission = Vec3(obj->material.emission);
-
 		if (emission.max() > 0.0f) {
 			if (mis_brdf_pdf > 0.0f) {
-				float solidAngle = abs(-ray.d.dot(obj->normal(hp))) * 400 * 400 / intersection.t / intersection.t;
-				color = color + (obj->material.emission)*attenuation*
-					mis_brdf_pdf * mis_brdf_pdf / (1 / solidAngle / solidAngle + mis_brdf_pdf * mis_brdf_pdf);
+				float solidAngle = abs(-ray.d.dot(obj->normal(hp))) * 100 * 100 / intersection.t / intersection.t;
+				color = color + (obj->material.emission)*attenuation *
+					mis_brdf_pdf * mis_brdf_pdf / (1 / solidAngle / solidAngle + mis_brdf_pdf * mis_brdf_pdf) *
+					exp(-density * intersection.t); // MEDIUM TERM REMOVE LATER
 				
 			} else {
-				color = color + emission * attenuation;
+				color = color + emission * attenuation
+					* exp(-density * intersection.t); // MEDIUM TERM REMOVE LATER
 			}
 		}
+		//float medium_t = -log(1.0f - uniform01(gen)) / density;
+		float medium_t = INFINITY;
+		if (intersection.t > medium_t) {
+			//hit medium
 
+			ray.o = medium_t * ray.d + ray.o;
+
+			// MIS direct light
+			Vec3 sampledLight = light->samplePoint(gen);
+			ray.d = (sampledLight - ray.o).normalized();
+			Intersection v = scene.castRay(ray);
+			if (light == v.hitObj) {
+				float pl = 1.0f / 100.0f / 100.0f;
+				float solidAngle = abs(-ray.d.dot(v.hitObj->normal(ray.o + v.t*ray.d))) / pl / v.t / v.t;
+
+				color = color + light->material.emission * attenuation * obj->material.albedo / M_PI * abs(n.dot(ray.d)) *
+					1 / solidAngle / (1 / solidAngle / solidAngle + abs(ray.d.dot(n)) / M_PI * abs(ray.d.dot(n)) / M_PI) *
+					exp(-density * v.t); // MEDIUM TERM REMOVE LATER
+			}
+
+			Vec3 d = ray.d;
+			ray.d = henyey_greenstein(ray.d, 0.1f, gen);
+			attenuation = attenuation * Vec3(0.999);
+			mis_brdf_pdf = henyey_greenstein_p(ray.d.dot(d), 0.1f);
+			continue;
+		}
+		ray.o = hp;
+
+		// TODO
 		// Some rays get eliminated before they can even do anything,
 		// So either try setting minimum bounces or rearranging some of the code
-		float bounce_probability = min(attenuation.max(), 0.9f);
+		float bounce_probability = min(attenuation.max(), 0.95f);
 		if (uniform01(gen) > bounce_probability) {
 			break;
 		}
@@ -259,30 +321,32 @@ Vec3 rayTrace(Ray& ray) {
 		if (obj->material.surface == reflective) {
 			float cos_t = ray.d.dot(n);
 
-			ray.d = (ray.d - (n * cos_t * 2.0f)).normalized();
+			ray.d = (ray.d - (n * cos_t * 2.0f));
 			attenuation = attenuation * obj->material.albedo;
 			mis_brdf_pdf = -1.0f;
 		} else if (obj->material.surface == diffuse) {
-			float pl = 1.0f / 400.0f / 400.0f;
-			if (true && light != obj) {
+			// TODO actually do this properly
+			float pl = 1.0f / 100.0f / 100.0f;
+			if (light != obj) {
 				Vec3 sampledLight = light->samplePoint(gen);
 				ray.d = (sampledLight - ray.o).normalized();
 				Intersection v = scene.castRay(ray);
 				if ( light == v.hitObj) {
 					float solidAngle = abs(-ray.d.dot(v.hitObj->normal(ray.o + v.t*ray.d)))/pl / v.t / v.t;
 
-					float pl = solidAngle * 1200.0f * 400.0f;
 					color = color + light->material.emission * attenuation * obj->material.albedo / M_PI * abs(n.dot(ray.d)) *
-						1 / solidAngle /( 1/solidAngle/solidAngle + abs(ray.d.dot(n))/M_PI* abs(ray.d.dot(n)) / M_PI);
+						1 / solidAngle /( 1/solidAngle/solidAngle + abs(ray.d.dot(n))/M_PI* abs(ray.d.dot(n)) / M_PI) *
+						exp(-density*v.t); // MEDIUM TERM REMOVE LATER
 				}
 			}
 			//color = color + emission * attenuation;
 			Matrix3 rotMatrix = rotMatrixVectors(n, Vec3(0.0f, 0.0f, 1.0f));
-			ray.d = rotMatrix * cosineSampleHemisphere();
+			ray.d = rotMatrix * cosineSampleHemisphere(gen);
 			float cos_t = abs(ray.d.dot(n));
 			mis_brdf_pdf = cos_t / M_PI;
 			attenuation = attenuation * obj->material.albedo;/**cos_t/(cos_t/M_PI )*/ // * pi (Surface area) / (pi (lambertian albedo constant))
 		} else if (obj->material.surface == specular) {
+			// TODO Be able to handle materials with different refractive indexes
 			float r = 1.0f / 1.5f;
 			float cos_t1 = -n.dot(ray.d);
 			if (cos_t1 < 0.0f) {
@@ -299,10 +363,10 @@ Vec3 rayTrace(Ray& ray) {
 				uniform01(gen) > ((r > 1.0f) ? schlickApprox(r, cos_t1) : schlickApprox(r, sqrt(1.0f - r * r*(1.0f - cos_t1 * cos_t1))))) {
 				// Refraction through the specular surface
 				float cos_t2 = sqrt(1.0f - r * r*(1.0f - cos_t1 * cos_t1));
-				ray.d = (r*ray.d + (r*cos_t1 - cos_t2)*n).normalized();
+				ray.d = (r*ray.d + (r*cos_t1 - cos_t2)*n);
 			} else {
 				// Reflection off the specular surface
-				ray.d = (ray.d + (n * cos_t1 * 2)).normalized();
+				ray.d = (ray.d + (n * cos_t1 * 2));
 			}
 			attenuation = attenuation * obj->material.albedo;
 			mis_brdf_pdf = -1.0f;
@@ -318,38 +382,41 @@ Vec3 rayTrace(Ray& ray) {
 	return color;
 }
 
+random_device rd;
+mt19937 gens[THREADS];
 auto t1 = chrono::high_resolution_clock::now();
 void render(Img &img) {
-
 	if (img.sample_count == 0) {
-		//#pragma omp parallel for schedule(dynamic) num_threads(2)
+		#pragma omp parallel for schedule(dynamic) num_threads(THREADS)
 		for (int py = 0; py < HEIGHT; py++) {
+			mt19937& gen = gens[omp_get_thread_num()];
 			for (int px = 0; px < WIDTH; px++) {
 				Ray ray = Ray(Vec3(), Vec3());
 				Vec3 colorVec = Vec3();
 				for (int sample = 0; sample < SAMPLES_PER_PIXEL; sample++) {
 					cameraRay(px + pix(gen), py + pix(gen), ray);
-					colorVec = colorVec + rayTrace(ray);
+					colorVec = colorVec + rayTrace(ray, gen);
 				}
 				(*img.pixels)[py][px] = colorVec/SAMPLES_PER_PIXEL;
 			}
 		}
 	} else {
-		//#pragma omp parallel for schedule(dynamic) num_threads(2)
+		#pragma omp parallel for schedule(dynamic) num_threads(THREADS)
 		for (int py = 0; py < HEIGHT; py++) {
+			mt19937& gen = gens[omp_get_thread_num()];
 			for (int px = 0; px < WIDTH; px++) {
 				Ray ray = Ray(Vec3(), Vec3());
 				Vec3 colorVec = Vec3();
 				for (int sample = 0; sample < SAMPLES_PER_PIXEL; sample++) {
 					cameraRay(px + pix(gen), py + pix(gen), ray);
-					colorVec = colorVec + rayTrace(ray);
+					colorVec = colorVec + rayTrace(ray, gen);
 				}
 				img.update(px, py, colorVec/SAMPLES_PER_PIXEL);
 			}
 		}
 
 	}
-
+		
 	// Performance metrics
 	float seconds = (chrono::duration_cast<std::chrono::microseconds>(chrono::high_resolution_clock::now() - t1).count() / 1000000.0);
 	cout << "Took " + to_string(seconds) + "s\n";
@@ -383,6 +450,7 @@ string getDateTime() {
 
 void render_loop(Img &img) {
 	t1 = chrono::high_resolution_clock::now();
+	for (int i = 0; i < THREADS; i++) gens[i] = mt19937(hash<int>{}(i));
 	while (true) {
 		render(img);
 		img.sample_count++;
@@ -418,6 +486,7 @@ void gui_thread(Img &img) {
 	while (window.isOpen()) {
 		sf::Event event;
 		while (window.pollEvent(event)) {
+			this_thread::sleep_for(100ms);
 			switch (event.type) {
 			case sf::Event::Closed:
 				window.close();
@@ -459,8 +528,6 @@ void gui_thread(Img &img) {
 		process_image(image, img.pixels);
 		texture.loadFromImage(image);
 		sprite.setTexture(texture);
-
-		//cout << "TSET" << (*img)[50][50] << " " << (unsigned int) image.getPixel(50,50).r << " " << (unsigned int)image.getPixel(50, 50).b<< endl;
 	}
 }
 
@@ -469,7 +536,10 @@ int main() {
 		cout << "Machine architecture must implement IEEE 754.\n";
 		return 0;
 	}
-
+	auto t1 = chrono::high_resolution_clock::now();
+	load_mesh("geometry/teapot.obj");
+	float seconds = (chrono::duration_cast<std::chrono::microseconds>(chrono::high_resolution_clock::now() - t1).count() / 1000000.0);
+	cout << "Took " + to_string(seconds) + "s\n";
 	// Create scene
 	//cout << "Choose scene (0-" << MAX_SCENES << "): ";
 	//int sceneIndex = 0;
